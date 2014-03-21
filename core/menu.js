78f0e66fcb94e70087be4c2b5ec53ba81c02feb8
Lucas Churchill <lucas@agtlucas.com>
2014-03-21 00:05:14 -0300

    Update menu

--- core/menu.js
+++ core/menu.js
@@ -1,1 +1,23 @@
 var display = require('display');
+
+var options = {};
+
+options.coffee = function () {
+  return 'Coffee';
+};
+
+options.coffeeWithSugar = function () {
+  return 'Coffee with sugar';
+};
+
+options.coffeeWithCream = function () {
+  return 'Coffee with cream';
+};
+
+options.coffeeWithCreamAndSugar = function () {
+  return 'Coffee with cream and sugar';
+};
+
+options.soup = function () {
+  return 'Soup';
+};
