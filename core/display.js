b55fa661c3a0636e103926e3b6e637363d1d0122
Lucas Churchill <lucas@agtlucas.com>
2014-03-29 22:12:29 -0300

    Update display

--- core/display.js
+++ core/display.js
@@ -2,9 +2,13 @@
 
 var display = {};
 
+display.price = function (price) {
+  console.log('Price: ' + price);
+};
+
 // I don't think this is necessary, but...
-display.balance = function () {
-  console.log('Balance...');
+display.balance = function (balance) {
+  console.log('Balance: ' + balance);
 };
 
 module.exports = display;
