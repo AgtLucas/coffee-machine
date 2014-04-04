681d09aed5ad4006088d0ea5d34ab63a0d80aa20
Lucas <lucas.silva@agenciamilagro.com.br>
2014-04-04 16:00:56 -0300

    Update menu

--- core/menu.js
+++ core/menu.js
@@ -1,25 +1,25 @@
 var display = require('display');
 
-var options = {};
+var menu = module.exports = {
 
-options.coffee = function () {
-  console.log('Coffee');
-};
+  coffee: function () {
+    console.log('Coffee');
+  };
 
-options.coffeeWithSugar = function () {
-  console.log('Coffee with sugar');
-};
+  coffeeWithSugar: function () {
+    console.log('Coffee with sugar');
+  };
 
-options.coffeeWithCream = function () {
-  console.log('Coffee with cream');
-};
+  coffeeWithCream: function () {
+    console.log('Coffee with cream');
+  };
 
-options.coffeeWithCreamAndSugar = function () {
-  console.log('Coffee with cream and sugar');
-};
+  coffeeWithCreamAndSugar: function () {
+    console.log('Coffee with cream and sugar');
+  };
 
-options.soup = function () {
-  console.log('Soup');
-};
+  soup: function () {
+    console.log('Soup');
+  };
 
-module.exports = menu;
+}
